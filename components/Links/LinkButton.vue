cbdad73ca234f086dc65f32753eec49cd1213178
Jacsiel Rodriguez <jacsielrj93@gmail.com>
2020-09-17 08:41:40 -0300

    add  new components

---
+++ components/Links/LinkButton.vue
@@ -0,0 +1,47 @@
+<template>
+    <a :href="to">
+    <button>
+        <slot></slot>
+    </button>
+    </a>
+</template>
+
+<style scoped>
+    a{
+        text-decoration:none;
+    }
+    button{
+        font-family: Mont;
+        font-style: normal;
+        font-weight: 800;
+        font-size: 15px;
+        line-height: 140%;
+        padding:8px 12px 8px 12px;
+        /* identical to box height, or 140% */
+        border:none;
+        letter-spacing: -0.02em;
+        background-color: #FF0C62;
+        color: #FFFFFF;
+        cursor: pointer;
+    }
+    button:active, button:focus{
+        border:none;
+        outline: none;
+    }
+</style>
+
+<script>
+export default {
+    props:['to'],
+    data(){
+        return {
+            colors:{
+                pink:{
+                    main:'#FF0C62',
+                    secondary:'#FFFFF'
+                }
+            }
+        }
+    }
+}
+</script>
---
+++ components/Links/LinkToTop.vue
@@ -0,0 +1,39 @@
+<template>
+    <button v-on:click="scrollTop()">
+        <IconArrowUpSimple/>
+    </button>
+</template>
+
+<style scoped>
+    button{
+        position: fixed;
+        right: 115px;
+        bottom: 15px;
+        font-family: Mont;
+        font-style: normal;
+        font-weight: 800;
+        font-size: 15px;
+        line-height: 140%;
+        padding:10px;
+        /* identical to box height, or 140% */
+        border:none;
+        letter-spacing: -0.02em;
+        background-color: #03002B;
+        color: #FFFFFF;
+        cursor: pointer;
+    }
+    button:active, button:focus{
+        border:none;
+        outline: none;
+    }
+</style>
+
+<script>
+export default {
+    methods:{
+        scrollTop(){
+            jQuery("html, body").animate({ scrollTop: 0 });
+        }
+    }
+}
+</script>
